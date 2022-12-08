285cc66af3089bb82d1a21851ba519145644c7ca
Tin <97665722+Tini79@users.noreply.github.com>
2022-12-08 22:29:33 +0700

    added delete action on kas

--- resources/views/pages/laporan/neraca/pdf.blade.php
+++ resources/views/pages/laporan/neraca/pdf.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+    <link rel="stylesheet" href="css/app.css">
+    <script type="text/javascript" src="js/app.js"></script>
 </head>
 
 <body class="bg-white">
